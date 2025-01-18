24f980e0821e87a193bb7f77306b194d50c1bde9
T44M <go.matsumiya@gmail.com>
2025-01-18 16:45:27 +0900

    change

--- Terraform/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "bucket_id" {
-  description = "S3バケットのID"
-  value       = aws_s3_bucket.my_bucket.id
-}
--- Terraform/variables.tf
+++
@@ -1,5 +0,0 @@
-variable "s3_bucket_name" {
-  description = "sellsy-terraform-state"
-  type        = string
-  default     = "tfstate-bucket"
-}
